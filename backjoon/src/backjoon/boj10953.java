7fd1ea2c785ae3a282ddc6dbddd36383d6dc0f13
sangyoungahn <tkddud1237@gmail.com>
2018-10-27 00:00:56 +0900

    2018.10.26(1)-edit

--- backjoon/src/backjoon/boj10953.java
+++ backjoon/src/backjoon/boj10953.java
@@ -3,10 +3,10 @@ package backjoon;
 import java.util.Scanner;
 
 public class boj10953 {
-	public void main (String args[]) {
+	public static void main (String args[]) {
 		Scanner sc = new Scanner(System.in);
 		int testCase = sc.nextInt();
-		//int sum[] = new int[testCase];
+		int sum[] = new int[testCase];
 		
 		for(int i = 0; i < testCase; i++) {
 			String s = sc.next();
@@ -15,7 +15,11 @@ public class boj10953 {
 			int a = Integer.parseInt(temp[0]);
 			int b = Integer.parseInt(temp[1]);
 			
-			System.out.println(a + b);
+			sum[i] = a + b;
+		}
+		
+		for(int i = 0; i < testCase; i++) {
+			System.out.println(sum[i]);
 		}
 	}
 }
