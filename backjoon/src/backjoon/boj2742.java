3fe89bbc4e85463676839ab2ea74dbad8a031aaf
sangyoungahn <tkddud1237@gmail.com>
2018-11-09 23:43:50 +0900

    input/output

--- backjoon/src/backjoon/boj2742.java
+++ backjoon/src/backjoon/boj2742.java
@@ -2,15 +2,17 @@ package backjoon;
 
 import java.util.Scanner;
 
-public class boj2741 {
-	public static void main (String args[]) {
+public class boj2742 {
+
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int input = sc.nextInt();
 		
 		if(input <= 100000) {
-			for(int i = 1; i <= input; i++) {
+			for(int i = input; i > 0; i--) {
 				System.out.println(i);
 			}
 		}
 	}
+
 }
