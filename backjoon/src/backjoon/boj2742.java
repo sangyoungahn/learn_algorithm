5734255f6595a1ae9fa06728e01296da53566dda
sangyoungahn <tkddud1237@gmail.com>
2018-11-11 05:30:55 +0900

    DP

--- backjoon/src/backjoon/boj2742.java
+++ backjoon/src/backjoon/boj2742.java
@@ -14,5 +14,4 @@ public class boj2742 {
 			}
 		}
 	}
-
 }
---
+++ backjoon/src/backjoon/boj9095.java
@@ -0,0 +1,29 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class boj9095 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int testCase = sc.nextInt();
+		int num;
+		
+		int[] arr = new int[11];
+		arr[1] = 1;		// 정수가 1일 때 가능한 경우의 수
+		arr[2] = 2;		// 정수가 2일 때 가능한 경우의 수
+		arr[3] = 4;		// 정수가 3일 때 가능한 경우의 수
+		
+		// 입력 받은 테스크 케이스만큼 반복
+		for(int i = 0 ; i <= testCase; i++) {
+			num = sc.nextInt();
+			
+			// 4이상부터는 점화식을 통해 값을 구함
+			for(int j = 4; j <= num; j++) {
+				arr[j] = arr[j-1] + arr[j-2] + arr[j-3];
+			}
+			System.out.println(arr[num]);
+		}
+		sc.close();
+	}
+}
