3dfb42807e44804cac0b329918a52cf4dd5761a2
sangyoungahn <tkddud1237@gmail.com>
2018-11-22 23:43:52 +0900

    input/output

---
+++ backjoon/src/backjoon/boj2522.java
@@ -0,0 +1,29 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class boj2522 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		int i, j;
+		
+		for (i = 0; i < input; i++) {
+			for (j = 0; j < input-i-1; j++)
+				System.out.print(" ");
+			for (j = 0; j <= i ; j++)
+				System.out.print("*");
+			
+			System.out.println();
+		}
+		
+		for (i = 1; i < input; i++) {		//행
+			for (j = 0; j < i; j++)			//공백
+				System.out.print(" ");
+			for (j = 0; j <= input-i-1 ; j++)
+				System.out.print("*");		//별
+			
+			System.out.println();
+		}
+	}
+}
